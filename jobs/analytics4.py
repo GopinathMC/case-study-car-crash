2a5636a725e720df4cf559e46ea9c3eeeaad45a6
Gopinath C <58396858+GopinathMC@users.noreply.github.com>
2021-08-08 14:53:55 +0530

    Add files via upload

--- jobs/analytics4.py
+++ jobs/analytics4.py
@@ -1,11 +1,12 @@
-from data_extracter import extract_data,extract_join_data,push_data
+from data_extracter import extract_data,push_data
 from pyspark.sql import Window
-from pyspark.sql.functions import col,row_number
+from pyspark.sql.functions import col,row_number,sum
+from pyspark.sql.types import IntegerType
 
 
 
 
-def transform_data(per_df,unit_df,spark):
+def transform_data(unit_df,spark):
     """
     Transform the raw data based on the business logic
 
@@ -14,13 +15,12 @@ def transform_data(per_df,unit_df,spark):
     :param spark: spark session object
     """
 
-    w = Window.orderBy(col('count').desc())
-    injur_df = per_df.filter("TOT_INJRY_CNT==1 or DEATH_CNT==1").select('CRASH_ID')
-    left_df = unit_df.filter(col('VEH_MAKE_ID')!='NA').select('CRASH_ID','VEH_MAKE_ID')
-    join_df = extract_join_data(left_df,injur_df,'inner','CRASH_ID')
-    veh_agg_df = join_df.groupBy('VEH_MAKE_ID').count()	
-
-    veh_most_inju_df = veh_agg_df.withColumn('rn',row_number().over(w)).filter("rn >=5 and rn <=15").select('VEH_MAKE_ID').withColumnRenamed('VEH_MAKE_ID','MOST_INJURED_VEH_MAKE_ID')
+    w = Window.orderBy(col('TOT_INJ_DEATH_CNT').desc())
+    sum_df = unit_df.filter(col('VEH_MAKE_ID')!='NA').withColumn('INJR_DEATH_CNT',col('DEATH_CNT')+col('TOT_INJRY_CNT')) \
+                    .withColumn('INJR_DEATH_CNT',col('INJR_DEATH_CNT').cast(IntegerType())).select('VEH_MAKE_ID','INJR_DEATH_CNT')
+    sum_df_agg = sum_df.groupBy('VEH_MAKE_ID').agg(sum(col('INJR_DEATH_CNT')).alias('TOT_INJ_DEATH_CNT'))
+    veh_most_inju_df = sum_df_agg.withColumn('rn',row_number().over(w)).filter("rn >=5 and rn <=15") \
+                                  .select('VEH_MAKE_ID').withColumnRenamed('VEH_MAKE_ID','MOST_INJR_DEATH_VEH_MAKE_ID')    
     return (
         veh_most_inju_df
         )
@@ -36,9 +36,8 @@ def run_job(spark,config,log):
     """	
 
     log.info('Extracting source csv files')
-    per_df = extract_data(spark,f"{config.get('source_data_path')}/Primary_Person_use.csv")
     unit_df = extract_data(spark,f"{config.get('source_data_path')}/Units_use.csv")
-    out_df = transform_data(per_df,unit_df,spark)
+    out_df = transform_data(unit_df,spark)
     log.info('All transformations done and writing to output path.......')
     push_data(out_df,f"{config.get('output_data_path')}/ANALYTICS4",config.get('write_mode'))
     log.info("File pushed successfully")
